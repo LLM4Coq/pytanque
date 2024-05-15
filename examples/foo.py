83fa09780212f57edbd1c4eb345d6a3cde2c6a0a
Guillaume Baudart <guillaume.baudart@inria.fr>
2024-05-15 16:52:07 +0200

    Example

--- examples/foo.py
+++ examples/foo.py
@@ -1,11 +1,12 @@
 from pytanque import Pytanque
 
-with Pytanque("127.0.0.1", 8765) as pet:
-    pet.start(file="./examples/foo.v", thm="addnC")
+with Pytanque("127.0.0.1", 8765, 0) as pet:
+    pet.init(root="./examples")
     pet.start(file="./examples/foo.v", thm="addnC")
     pet.run_tac("induction n.")
+    pet.goals()
+    pet.premises()
     pet.run_tac("simpl.")
-    pet.run_tac("auto.")
     pet.rollback()
     pet.rollback()
     pet.run_tac("by elim: n => //= ? ->.")
