3c099fa8e1b6a10a4f3c9e35c6c2702aeed79ef2
Guillaume Baudart <guillaume.baudart@inria.fr>
2024-07-12 15:37:48 +0200

    WIP Schema+BFS

--- pytanque/schemas.py
+++ pytanque/schemas.py
@@ -1,6 +1,7 @@
 from .protocol import State, Opts
 from .client import Pytanque, PetanqueError
-from typing import Optional, List
+from .search import BFS
+from typing import Optional, List, Callable
 import re
 from dataclasses import dataclass, field
 
@@ -64,6 +65,7 @@ class Schema:
     tactics: List[str] = field(default_factory=list)
     admit_idx: List[int] = field(default_factory=list)
     admit_states: List[State] = field(default_factory=list)
+    admit_errors: List[str | None] = field(default_factory=list)
 
     def __repr__(self):
         steps = map(
@@ -102,16 +104,23 @@ def fix_proof(
                 r"Coq: \[Focus\] Wrong bullet (?:\++|\-+|\*+): Expecting (?P<bullet>\++|\-+|\*+)",
                 err.message,
             ):  # refocus on correct bullet
-                return fix(state, idx, [m.group("bullet"), "admit."] + tactics, False)
+                return fix(state, idx, [m.group("bullet")] + tactics, False)
+            if m := re.match(
+                r"Coq: No such goal. Focus next goal with bullet (?P<bullet>\++|\-+|\*+)",
+                err.message,
+            ):  # refocus on correct bullet
+                return fix(state, idx, [m.group("bullet")] + tactics, False)
             if re.match(
                 r"Coq: \[Focus\] Wrong bullet (?:\++|\-+|\*+): Current bullet (?:\++|\-+|\*+) is not finished.",
                 err.message,
             ):  # close previous subgoal and retry.
+                schema.admit_errors.append(err.message)
                 return fix(state, idx, ["admit."] + tactics, False)
             if re.match(
                 r"Coq: This proof is focused, but cannot be unfocused this way",
                 err.message,
             ):  # close current goal and try to unfocus
+                schema.admit_errors.append(err.message)
                 return fix(state, idx, ["admit."] + tactics, False)
             if re.match(
                 r"Coq: \[Focus\] Wrong bullet (?:\++|\-+|\*+): No more goals.",
@@ -119,13 +128,14 @@ def fix_proof(
             ):  # Drop bullet
                 return fix(state, idx, tactics[1:], True)
             else:  # replace tac by admit and drop until next valid tactic.
+                schema.admit_errors.append(err.message)
                 return fix(state, idx, ["admit."] + tactics[1:], True)
 
     tactics = split_proof(proof)
     schema = fix(state, 0, tactics, False)
     match schema.tactics:  # remove nested admit.
         case ["{", "admit.", "}"]:
-            schema.tactics = ["admits"]
+            schema.tactics = ["admit."]
             schema.admit_idx = [0]
     return schema
 
@@ -177,6 +187,41 @@ What is the proof?
 """
 
 
+def tactics_prompt(goal: str, num_samples: int) -> str:
+    return f"""
+Let's play a game.
+We are trying to prove a theorem using the Coq proof assistant.
+I will give you the current state of the prover as a code block, and you answer with a list of {num_samples} possible tactics, ordered from the most promising one to the least promising one. Each tactics should be associated to a confidence score, i.e., a positive number.
+
+For instance, here is a possible state:
+
+```
+n  : nat
+IHn  : 2 * n = n + n
+-----------------------------
+2 * S n = S n + S n
+```
+
+And here is a possible answer:
+
+```
+[("lia.", 0.8), ("auto.", 0.6), ("simpl.", 0.4), ...]
+```
+
+Answer with a single code block.
+Ready? Take a deep breath.
+
+Here is your goal:
+
+```
+{goal}
+```
+"""
+
+
+import ast
+
+
 class GPTAgent:
     def __init__(self, pet: Pytanque):
         self.pet = pet
@@ -184,11 +229,10 @@ class GPTAgent:
         self.schema = Schema()
         self.proof = []
 
-    def ask_gpt(self, prompt: str) -> str:
+    def ask_gpt(self, prompt: str, n: int = 1) -> str:
         self.messages.append({"role": "user", "content": prompt})
         resp = client.chat.completions.create(
-            messages=self.messages,
-            model="gpt-4o",
+            messages=self.messages, model="gpt-4o", n=n
         )
         content = resp.choices[0].message.content
         if not content:
@@ -196,14 +240,48 @@ class GPTAgent:
         self.messages.append({"role": "assistant", "content": content})
         return content
 
-    def start(self, context, state):
+    def start(self, context, state) -> Schema:
         current_goal = self.pet.goals(state).goals[0].pp()
         proof = self.ask_gpt(init_prompt(context, current_goal))
-        print(f"\nAssistant:\n\n {proof}")
         self.schema = fix_proof(self.pet, state, proof)
         return self.schema
 
-    def next(self):
+    def schema_generator(self, state) -> str:
+        current_goal = self.pet.goals(state).goals[0].pp()
+        prompt = next_prompt(current_goal)
+        return self.ask_gpt(prompt)
+
+    def bfs_generator(self, state) -> str:
+
+        num_samples = 10
+        max_iters = 10
+
+        def gen(state, num_samples):
+            current_goal = self.pet.goals(state).goals[0].pp()
+            code = self.ask_gpt(tactics_prompt(current_goal, num_samples))
+
+            # Remove code block delimiter
+            code = code.strip()
+            if code.startswith("```"):
+                code = code[3:]
+            if code.endswith("```"):
+                code = code[:-3]
+            code = code.strip()
+
+            tactics, scores = list(zip(*ast.literal_eval(code)))
+            return (
+                list(map(lambda t: f"{t}{'.' if t[-1] != '.' else ''}", tactics)),
+                scores,
+            )
+
+        bfs = BFS(max_iters, num_samples, pet=self.pet, tactic_generator=gen)
+        try:
+            proof = bfs.search(state)
+            return " ".join(proof)
+        except:
+            return "admit."
+
+    def next(self, proof_generator: Callable[[State], str]) -> Schema:
         if not self.schema:
             raise PetanqueError(0, "Undefined schema")
 
@@ -211,35 +289,53 @@ class GPTAgent:
 
         schema = Schema()
 
-        def update(
-            i: int, offset: int, admit_states: List[State], admit_idx: List[int]
-        ) -> Schema:
-            if not admit_states:
-                schema.tactics += self.schema.tactics[i + 1 :]
-                return schema
+        offset = 0
+        p_ai = -1
 
-            next_state = admit_states[0]
-            next_i = admit_idx[0]
-            current_goal = self.pet.goals(next_state).goals[0].pp()
-            prompt = next_prompt(current_goal)
-            print(f"\nUser:\n\n {prompt}")
-            sub_proof = self.ask_gpt(prompt)
-            print(f"\nAssistant:\n\n {sub_proof}")
-            sub_schema = fix_proof(self.pet, next_state, sub_proof)
-            print(f"\nSubproof:\n{sub_schema}")
+        for state, ai, err in zip(
+            self.schema.admit_states, self.schema.admit_idx, self.schema.admit_errors
+        ):
 
-            schema.tactics += self.schema.tactics[i + 1 : next_i] + sub_schema.tactics
+            sub_proof = proof_generator(state)
+            print(f"Trying:\n {sub_proof}\n")
+            sub_schema = fix_proof(self.pet, state, sub_proof)
+            print(f"Got:\n {sub_schema}\n")
+
+            schema.tactics += self.schema.tactics[p_ai + 1 : ai] + sub_schema.tactics
             schema.admit_states += sub_schema.admit_states
-            schema.admit_idx += [next_i + offset + k for k in sub_schema.admit_idx]
-            return update(
-                next_i,
-                offset + len(sub_schema.tactics) - 1,
-                admit_states[1:],
-                admit_idx[1:],
-            )
+            schema.admit_errors += sub_schema.admit_errors
+            schema.admit_idx += [ai + offset + k for k in sub_schema.admit_idx]
 
-        next_schema = update(-1, 0, self.schema.admit_states, self.schema.admit_idx)
-        if next_schema.tactics == self.schema.tactics:
+            offset += len(sub_schema.tactics) - 1
+            p_ai = ai
+
+        schema.tactics += self.schema.tactics[p_ai + 1 :]
+
+        if schema.tactics == self.schema.tactics:
             raise PetanqueError(0, "No proof found")
-        self.schema = next_schema
+        self.schema = schema
         return self.schema
+
+    def recursive_search(
+        self, context, file_name: str, thm_name: str, max_depth: int = 5
+    ) -> Schema:
+
+        state = self.pet.start(file=file_name, thm=thm_name)
+        self.start(context, state)
+        print(f"Initial schema\n{self.schema}\n")
+
+        def search(depth: int) -> Schema:
+            if depth == 0:
+                raise PetanqueError(0, "No proof found")
+            if not self.schema.admit_states:
+                return self.schema
+            self.next(self.schema_generator)
+            return search(depth - 1)
+
+        return search(max_depth)
+
+    def schema_best_first(self, context, file_name: str, thm_name: str):
+        state = self.pet.start(file=file_name, thm=thm_name)
+        self.start(context, state)
+        print(f"Initial schema\n{self.schema}\n")
+        return self.next(self.bfs_generator)
